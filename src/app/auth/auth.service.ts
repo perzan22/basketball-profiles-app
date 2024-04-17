82ef004bc102454cad9152de9bac99326cccd66a
olafperzanowski <olafperzanowski@gmail.com>
2024-04-18 01:15:29 +0200

    Added login function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,12 +2,33 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthData } from "./auth-data.model";
+import { Subject } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
+    private token!: string;
+    private isAuth = false;
+    private userID!: string;
+    private authStatusListener = new Subject<boolean>();
+
     constructor(private http: HttpClient, private router: Router) {}
 
+    getToken() {
+        return this.token;
+    }
+
+    getIsAuth() {
+        return this.isAuth;
+    }
+
+    getUserId() {
+        return this.userID;
+    }
+
+    getAuthStatusListener() {
+        return this.authStatusListener.asObservable();
+    }
 
     createUser(email: string, password: string, name: string, surname: string) {
         const authData: AuthData = { email: email, password: password, name: name, surname: surname };
@@ -17,4 +38,22 @@ export class AuthService {
             }
         })
     }
+
+    login(email: string, password: string) {
+        const authData = { email: email, password: password }
+        this.http.post<{ token: string, userID: string }>('http://localhost:3000/api/users/login', authData).subscribe({
+            next: response => {
+                const token = response.token;
+                this.token = token;
+                if(token) {
+                    this.isAuth = true;
+                    this.userID = response.userID;
+                    this.authStatusListener.next(true);
+                    this.router.navigate(['/']);
+                }
+            }
+        })
+    }
+
+    
 }

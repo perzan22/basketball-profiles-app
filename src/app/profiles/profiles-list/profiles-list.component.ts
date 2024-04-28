9d399b8f072044130ff470158b549c5c3393ed3a
olafperzanowski <olafperzanowski@gmail.com>
2024-04-29 00:58:50 +0200

    commit

--- src/app/profiles/profiles-list/profiles-list.component.ts
+++ src/app/profiles/profiles-list/profiles-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Profile } from '../profile.model';
 import { ProfileService } from '../profile.service';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-profiles-list',
@@ -13,8 +14,11 @@ export class ProfilesListComponent implements OnInit, OnDestroy{
 
   profiles: Profile[] = [];
   private profileSubs!: Subscription;
+  public isAuth: boolean = false
+  userID!: String
+  private authStatusSubs!: Subscription;
 
-  constructor(private profileService: ProfileService) {}
+  constructor(private profileService: ProfileService, private authService: AuthService) {}
 
   ngOnInit(): void {
     
@@ -22,14 +26,22 @@ export class ProfilesListComponent implements OnInit, OnDestroy{
     this.profileSubs = this.profileService.getProfilesUpdateListener().subscribe({
       next: profileData => {
         this.profiles = profileData.profiles;
-        console.log(this.profiles)
+
       }
     })
-    
+    this.userID = this.authService.getUserId();
+    this.isAuth = this.authService.getIsAuth();
+    this.authStatusSubs = this.authService.getAuthStatusListener().subscribe({
+      next: isAuthenticated => {
+        this.isAuth = isAuthenticated.isAuth
+        this.userID = this.authService.getUserId();
+      }
+    })
   }
 
   ngOnDestroy(): void {
     this.profileSubs.unsubscribe();
+    this.authStatusSubs.unsubscribe();
   }
 
   onDelete(profileID: string) {
